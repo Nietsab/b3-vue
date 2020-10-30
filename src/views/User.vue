b913449f1bbc88d66bf9caaae25bfda24bfd5f9e
Nietsab <xrenkin78x@gmail.com>
2020-10-30 12:24:47 +0100

    30/10/2020

--- src/views/User.vue
+++ src/views/User.vue
@@ -15,31 +15,67 @@
 <script>
 
 
-export default {
-  name: 'User',
-  components : {
-  },
-  props: {
-    msg: String,
-    saucisse : String
+  export default {
+    name: 'User',
+    components : {
+    },
+    props: {
+      msg: String,
+      saucisse : String
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
+
+
+
+
+
+<style lang="scss">
+  // B_E-M
+  /*
+
+  priorité CSS : C'est le plus précis qui gagne
+  - precision basse : tag <h3>
+  - precision moyenne : classes .classe
+  - precision haute : identifiant id="xxx"
+  - override : ligne de définition de style qui fini par !important
+   */
+  // Variable
+  $maCouleurDeBase: blueviolet;
+  // mixins
+  @mixin highlightedText {
+    font-size: 48rem;
   }
-  ul {
-    list-style-type: none;
-    padding: 0;
+
+  // function/mixins
+  @mixin size-text($size : 8)  {
+    font-size: $size rem;
+    margin: $size/2 px;
+    box-shadow: $size/2, $size/2, $size/2, rgba(0,0,0,.25);
   }
-  li {
-    display: inline-block;
-    margin: 0 10px;
+
+  // /!\ en SASS/SCSS les noms de variables ou mixins ou function ne sont pas la dif
+  // $mon-poney <==> $mon_poney
+
+  .user{ // block
+
+    h3 {
+      color: $maCouleurDeBase;
+      @include highlightedText;
+      @include size-text(10);
+
+      &.prout{ // h3.prout
+        font-weight: 600 ;
+      }
+    }
+
+    h2{
+      @extend h3;
+      @include size-text(9);
+    }
+
   }
-  a {
-    color: #42b983;
-  }
+
 </style>
